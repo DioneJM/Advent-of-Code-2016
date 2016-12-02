fefcae5bfe5df67bcb331ff0fa5e51efef538135
DM <dionemorales@outlook.com>
2016-12-02 23:56:39 +1100

    Completed solution for Day 2 part 2

--- 2/Day2Part2.scala
+++ 2/Day2Part2.scala
@@ -1,6 +1,5 @@
 import scala.io.Source
 
-//Not working
 object Day2Part2 {
   def main(args: Array[String]) {
     println("Day 2 solution")
@@ -12,33 +11,31 @@ object Day2Part2 {
     var curInstruction = instructions(instr)
       for(c <- 0 to curInstruction.length - 1) { //Move through the keypad
         var direction = curInstruction(c)
-        println("cur = " + curButton)
 
         direction match {
           case 'U' => {
             if(curButton == 3 || curButton == 13)
               curButton -= 2
-            if(curButton == 6 || curButton == 7 || curButton == 8 || curButton == 10 || curButton == 11 || curButton == 12)
+            else if(curButton == 6 || curButton == 7 || curButton == 8 || curButton == 10 || curButton == 11 || curButton == 12)
               curButton -= 4
           }
           case 'D' => {
             if(curButton == 1 || curButton == 11)
-              curButton += 2
-            if(curButton == 2 || curButton == 3 || curButton == 4 || curButton == 6 || curButton == 7 || curButton == 8)
+                curButton += 2
+            else if(curButton == 2 || curButton == 3 || curButton == 4 || curButton == 6 || curButton == 7 || curButton == 8)
               curButton += 4
           }
           case 'L' => {
-            if(curButton != 1 || curButton != 2 || curButton != 5 || curButton != 10 || curButton != 13)
+            if(curButton != 1 && curButton != 2 && curButton != 5 && curButton != 10 && curButton != 13)
               curButton -= 1
           }
           case 'R' => {
-            if(curButton != 1 || curButton != 4 || curButton != 9 || curButton != 12 || curButton != 13)
+            if(curButton != 1 && curButton != 4 && curButton != 9 && curButton != 12 && curButton != 13)
               curButton += 1
           }
         }
-        print(direction + " -> " +curButton + " ")
+
       }
-      println()
       println(curButton)
       curButton match {
         case 10 => password2 = password2 + 'A'
